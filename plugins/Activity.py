85a2cf1bbb141186060f089d952798a2850aef21
Mikk155 <martimsanchez155@gmail.com>
2024-10-03 19:56:57 -0300

    Implement Activity

---
+++ plugins/Activity.py
@@ -0,0 +1,44 @@
+from plugins.main import *
+
+hooks = [
+    Hooks.on_think
+];
+
+RegisterHooks( __file__, hook_list=hooks );
+
+global Activity;
+Activity = jsonc( '{}\\plugins\\Activity.json'.format( abspath ) );
+
+global Activity_Time;
+Activity_Time = 0;
+
+global LastState;
+LastState = 1;
+
+async def on_think():
+
+    global Activity_Time;
+
+    if Activity_Time < gpGlobals.time:
+
+        global Activity;
+        global LastState;
+
+        if LastState > len( Activity[ "State" ] ):
+            LastState = 1;
+
+        try:
+            pActivity = discord.Activity(
+                type = discord.ActivityType.listening,
+                name = config[ "prefix" ],
+                state = Activity[ "State" ][ LastState - 1 ],
+            );
+
+            await bot.change_presence( activity = pActivity );
+
+        except:
+            pass;
+
+        LastState += 1;
+
+        Activity_Time = gpGlobals.time + Activity[ "IntervalUpdate" ];
