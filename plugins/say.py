e0499cd1838b72f5f20dc4db230506790f87d6b6
Mikk155 <martimsanchez155@gmail.com>
2024-10-05 02:02:07 -0300

    Update say.py

--- plugins/say.py
+++ plugins/say.py
@@ -9,14 +9,11 @@ from plugins.main import *
 async def say( interaction: discord.Interaction, message: str, hidename: bool = True ):
     """Make the bot say something"""
 
-    await interaction.response.send_message( '<:walter_what:1278078147870331011>', ephemeral=True, delete_after=0.1 )
-
     try:
         if hidename and interaction.user.guild_permissions.administrator:
+            await interaction.response.send_message( '<:walter_what:1278078147870331011>', ephemeral=True, delete_after=0.1 )
             await interaction.channel.send( message );
         else:
             await interaction.response.send_message( message );
     except Exception as e:
         await interaction.response.send_message( "Exception: {}".format( e ), ephemeral=True );
-
-        await message.channel.send( "You have to specify a text" );
