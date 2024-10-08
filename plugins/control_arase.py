186bea17fa1a258fda57deb8d8c9eccb2d8d87fb
Mikk155 <martimsanchez155@gmail.com>
2024-10-08 00:05:01 -0300

    Control horny arase

---
+++ plugins/control_arase.py
@@ -0,0 +1,31 @@
+from plugins.main import *
+
+async def on_message( message: discord.Message ):
+
+    if message.author.id == 768337526888726548 and message.guild and message.guild.id == 744769532513615922:
+
+        try:
+
+            avatar = None
+
+            user = await bot.fetch_user( 121735805369581570 );
+
+            avatar = user.avatar.url;
+
+            for h in [ 'mommy', 'sex', 'gf', 'feet', 'armpit' ]:
+
+                if h in message.content:
+
+                    webhook = await message.channel.create_webhook( name='KernCore' );
+
+                    await webhook.send( content='Control yourself', username='KernCore', avatar_url=avatar );
+
+                    await webhook.delete()
+
+                    break;
+
+        except Exception as e:
+
+            return Hook.Continue();
+
+    return Hook.Continue();
