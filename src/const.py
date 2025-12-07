2d043c23aa626dfbea539a29b5f6d92e9069ddfd
Mikk155 <martimsanchez155@gmail.com>
2025-12-07 12:45:22 -0300

    Add a global testing bool for the test server

--- src/const.py
+++ src/const.py
@@ -4,17 +4,19 @@ from src.utils.RGB import RGB;
 
 MikkID: int = 744768007892500481;
 
+g_Testing = True;
+
 class LimitlessPotential:
 
-    id: int = 744769532513615922;
+    id: int = 1145236064596918304 if g_Testing else 744769532513615922;
 
     class Channels( IntEnum ):
         Announcements: int = 842129545494134785;
         Users: int = 1343203830787080203;
-        BotLogs: int = 1343209173969535108;
+        BotLogs: int = 1343235842822504481 if g_Testing else 1343209173969535108;
         GreetTheGafther: int = 1376757390757724298;
         Trusted: int = 1343197825751715851;
-        DiscordLogs: int = 1343203478742241372;
+        DiscordLogs: int = 1343235788623708350 if g_Testing else 1343203478742241372;
         BetaTesting: int = 1403853307629277194;
         CountTogether: int = 1343206179878211755;
         LPMemes: int = 1343244435583926323;
@@ -23,13 +25,13 @@ class LimitlessPotential:
 
 class TheCult:
 
-    id: int = 1216162825307820042;
+    id: int = 1145236064596918304 if g_Testing else 1216162825307820042;
 
     class Channels( IntEnum ):
         Announcements: int = 1397367866667434125;
         Benefactors: int = 1216208600725196922;
         Users: int = 1447021578532425802;
-        DiscordLogs: int = 1397355050715578439;
+        DiscordLogs: int = 1343235788623708350 if g_Testing else 1397355050715578439;
 
     Invites: dict[ str, int ] = {};
 
