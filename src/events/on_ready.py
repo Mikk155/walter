4dd91e9d69dfe78e78035ea0f4383d9f29dfed27
Mikk155 <martimsanchez155@gmail.com>
2025-12-30 21:12:21 -0300

    Always refresh invites list

--- src/events/on_ready.py
+++ src/events/on_ready.py
@@ -11,30 +11,30 @@ async def on_ready() -> None:
 
     await bot.wait_until_ready();
 
+    async def getInvites( target: TheCult | LimitlessPotential ) -> None:
+    #
+        targetGuild: discord.Guild = bot.get_guild( target.id );
+
+        if targetGuild:
+        #
+            targetInvites: list[discord.Invite] = await targetGuild.invites();
+
+            if targetInvites:
+            #
+                for invite in targetInvites:
+                #
+                    target.Invites[ invite.code ] = invite.uses;
+                #
+            #
+        #
+    #
+
+    await getInvites( TheCult );
+    await getInvites( LimitlessPotential );
+
     if bot.__Started__ is False: # This is the first time the bot is running. since on_ready is called multiple times
     #
         await bot.get_channel( LimitlessPotential.Channels.BotLogs ).send( f"Connected and logged as {bot.user.name}#{bot.user.discriminator}", silent=True );
-
-        async def getInvites( target: TheCult | LimitlessPotential ) -> None:
-        #
-            targetGuild: discord.Guild = bot.get_guild( target.id );
-
-            if targetGuild:
-            #
-                targetInvites: list[discord.Invite] = await targetGuild.invites();
-
-                if targetInvites:
-                #
-                    for invite in targetInvites:
-                    #
-                        target.Invites[ invite.code ] = invite.uses;
-                    #
-                #
-            #
-        #
-
-        await getInvites( TheCult );
-        await getInvites( LimitlessPotential );
 
         bot.__Started__ = True;
     #
